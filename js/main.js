a9db732eb488829d9fbdb776a0c2880fe4f43c4f
Maria Vlasova <vlasova_ya@mail.ru>
2020-09-24 21:13:58 +0400

    module3-task1
    Displays photos of other users

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const PHOTOS_AMOUNT = 25;
+const PICTURES_AMOUNT = 25;
 const USER_NAMES = [
   'Человек-паук',
   'Халк',
@@ -17,29 +17,28 @@ const COMMENTS = [
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
+const MIN_COMMENTS_AMOUNT = 0;
 const MAX_COMMENTS_AMOUNT = 6;
 const MIN_AVATAR_AMOUNT = 1;
 const MAX_AVATAR_AMOUNT = 6;
 const MIN_LIKES = 15;
 const MAX_LIKES = 200;
 
-let picturesElement = document.querySelector('.pictures');
-const picturesTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
-
 // Возвращает результат, включая максимум и минимум
-let getRandomIntInclusive = function (min, max) {
+const getRandomIntInclusive = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 // Возвращает случайный элемент массива
-let getRandomArrayElement = function (array) {
+const getRandomArrayElement = function (array) {
   return array[getRandomIntInclusive(0, array.length - 1)];
 };
 
-let createComments = function (commentsArray) {
-  let comments = [];
+// Создает массив комментариев
+const createComments = function (commentsArray) {
+  const comments = [];
   for (let i = 0; i < commentsArray.length; i++) {
     comments[i] = {
       avatar: 'img/avatar-' + getRandomIntInclusive(MIN_AVATAR_AMOUNT, MAX_AVATAR_AMOUNT) + '.svg',
@@ -47,19 +46,44 @@ let createComments = function (commentsArray) {
       name: getRandomArrayElement(USER_NAMES)
     };
   }
-
   return comments;
 };
 
-let createPhotoDescription = function (descriptionsArray) {
-  let photoDescriptions = [];
-  for (let i = 0; i < descriptionsArray.length; i++) {
+// Создает массив описаний фото
+const createPhotoDescription = function (descriptionsArray) {
+  const photoDescriptions = [];
+  for (let i = 0; i < descriptionsArray; i++) {
     photoDescriptions[i] = {
-      url: 'photos/' + i + '.jpg',
+      url: 'photos/' + (i + 1) + '.jpg',
       description: 'описание фотографии',
       likes: getRandomIntInclusive(MIN_LIKES, MAX_LIKES),
-      comments: createComments(getRandomIntInclusive(0, MAX_COMMENTS_AMOUNT))
+      comments: createComments(getRandomIntInclusive(MIN_COMMENTS_AMOUNT, MAX_COMMENTS_AMOUNT))
     };
   }
   return photoDescriptions;
 };
+
+const picturesItem = document.querySelector('.pictures');
+const picturesTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
+
+// Копирует шаблон и добавляет в него данные
+const createPicture = function (picture) {
+  const pictureElement = picturesTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = picture.url;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+
+  return pictureElement;
+};
+
+// Cоздаем и заполняет DOM-элементы
+const createPicturesList = function (pictures) {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < pictures.length; i++) {
+    fragment.appendChild(createPicture(pictures[i]));
+  }
+  picturesItem.appendChild(fragment);
+};
+
+// Отображает фото пользователей
+createPicturesList((createPhotoDescription(PICTURES_AMOUNT)));
