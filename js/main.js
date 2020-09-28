2358893adf12ad7548c7098cbcb5701f788ad2c2
Maria Vlasova <vlasova_ya@mail.ru>
2020-09-28 09:55:56 +0400

    module3-task2
    fixes bugs after review

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@
 
 const PICTURES_AMOUNT = 25;
 const USER_NAMES = [
-  'Человек-паук',
-  'Халк',
-  'Доктор Стрэндж',
-  'Сорвиголова',
-  'Локи',
-  'Дэдпул'
+  `Человек-паук`,
+  `Халк`,
+  `Доктор Стрэндж`,
+  `Сорвиголова`,
+  `Локи`,
+  `Дэдпул`
 ];
 const COMMENTS = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  `Всё отлично!`,
+  `В целом всё неплохо. Но не всё.`,
+  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+  `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
+  `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+  `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
 ];
 const CommentsAmount = {
   MIN: 0,
@@ -59,7 +59,7 @@ const createPhotoDescription = (descriptions) => {
   for (let i = 0; i < descriptions; i++) {
     photoDescriptions.push({
       url: `photos/${(i + 1)}.jpg`,
-      description: 'описание фотографии',
+      description: `описание фотографии`,
       likes: getRandomIntInclusive(LikesAmount.MIN, LikesAmount.MAX),
       comments: createComments(getRandomIntInclusive(CommentsAmount.MIN, CommentsAmount.MAX))
     });
@@ -67,15 +67,15 @@ const createPhotoDescription = (descriptions) => {
   return photoDescriptions;
 };
 
-const picturesItem = document.querySelector('.pictures');
-const picturesTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesItem = document.querySelector(`.pictures`);
+const picturesTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
 
 // Копирует шаблон и добавляет в него данные
 const createPicture = (picture) => {
   const pictureElement = picturesTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = picture.url;
-  pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
-  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+  pictureElement.querySelector(`.picture__img`).src = picture.url;
+  pictureElement.querySelector(`.picture__comments`).textContent = picture.comments.length;
+  pictureElement.querySelector(`.picture__likes`).textContent = picture.likes;
   return pictureElement;
 };
 
@@ -92,48 +92,47 @@ const createPicturesList = (pictures) => {
 const pictures = createPhotoDescription(PICTURES_AMOUNT);
 createPicturesList(pictures);
 
-const commentsAmount = document.querySelector('.social__comments');
-const commentsItem = commentsAmount.querySelector('.social__comment');
-const bigPictureItem = document.querySelector('.big-picture');
+const commentsContainer = document.querySelector(`.social__comments`);
+const commentsItem = commentsContainer.querySelector(`.social__comment`);
+const bigPictureItem = document.querySelector(`.big-picture`);
 
 // Создает один комментарий
-const createCommentItem = (data) => {
+const createCommentItem = (commentData) => {
   const commentElement = commentsItem.cloneNode(true);
 
-  commentElement.querySelector('.social__picture').src = data.avatar;
-  commentElement.querySelector('.social__picture').alt = data.name;
-  commentElement.querySelector('.social__text').textContent = data.message;
+  commentElement.querySelector(`.social__picture`).src = commentData.avatar;
+  commentElement.querySelector(`.social__picture`).alt = commentData.name;
+  commentElement.querySelector(`.social__text`).textContent = commentData.message;
 
   return commentElement;
 };
 
-// Cоздает и заполняет DOM-элементы
-const createCommentsAmount = (data) => {
-  commentsAmount.innerHTML = '';
+// Создает и заполняет DOM-элементы
+const createCommentsFragment = (commentData) => {
+  commentsContainer.innerHTML = ``;
   const fragment = document.createDocumentFragment();
-  for (let i = 0; i < data.length; i++) {
-    fragment.appendChild(createCommentItem(data[i]));
+  for (let i = 0; i < commentData.length; i++) {
+    fragment.appendChild(createCommentItem(commentData[i]));
   }
-  commentsAmount.appendChild(fragment);
+  commentsContainer.appendChild(fragment);
 };
 
 // Отрисовывает большое фото
-const renderBigPictureItem = (data) => {
-  bigPictureItem.classList.remove('hidden');
+const renderBigPictureItem = (pictureData) => {
+  bigPictureItem.classList.remove(`hidden`);
 
-  bigPictureItem.querySelector('.big-picture__img img').src = data.url;
-  bigPictureItem.querySelector('.likes-count').textContent = data.likes;
-  bigPictureItem.querySelector('.social__caption').textContent = data.description;
-  bigPictureItem.querySelector('.comments-count').textContent = data.comments.length;
+  bigPictureItem.querySelector(`.big-picture__img img`).src = pictureData.url;
+  bigPictureItem.querySelector(`.likes-count`).textContent = pictureData.likes;
+  bigPictureItem.querySelector(`.social__caption`).textContent = pictureData.description;
+  bigPictureItem.querySelector(`.comments-count`).textContent = pictureData.comments.length;
 
-  bigPictureItem.querySelector('.social__comment-count ').classList.add('hidden');
-  bigPictureItem.querySelector('.comments-loader').classList.add('hidden');
+  bigPictureItem.querySelector(`.social__comment-count `).classList.add(`hidden`);
+  bigPictureItem.querySelector(`.comments-loader`).classList.add(`hidden`);
 
-  createCommentsAmount(data.comments);
+  createCommentsFragment(pictureData.comments);
 };
 
 // Показывает первую фотографию из массива объектов
 renderBigPictureItem(pictures[0]);
 
-const bodyTag = document.querySelector('body');
-bodyTag.classList.add('modal-open');
+document.querySelector(`body`).classList.add(`modal-open`);
