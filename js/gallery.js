e60cbdd540dd79e47eca5f190f0a47d182c50151
Maria Vlasova <vlasova_ya@mail.ru>
2020-10-09 00:31:57 +0400

    module5-task1
    divides into modules

---
+++ js/gallery.js
@@ -0,0 +1,58 @@
+'use strict';
+
+(() => {
+  const picturesItem = document.querySelector(`.pictures`);
+  const picturesTemplate = document.querySelector(`#picture`).content.querySelector(`.picture`);
+
+  // Копирует шаблон и добавляет в него данные
+  const createPicture = (picture) => {
+    const pictureElement = picturesTemplate.cloneNode(true);
+    pictureElement.querySelector(`.picture__img`).src = picture.url;
+    pictureElement.querySelector(`.picture__comments`).textContent = picture.comments.length;
+    pictureElement.querySelector(`.picture__likes`).textContent = picture.likes;
+    return pictureElement;
+  };
+
+  // Создает и заполняет DOM-элементы
+  const createPicturesList = (pictures) => {
+    const fragment = document.createDocumentFragment();
+    for (let picture of pictures) {
+      const createdPicture = createPicture(picture);
+      fragment.appendChild(createdPicture);
+      createdPicture.addEventListener(`click`, () => {
+        window.preview.showBigPicture(picture);
+      });
+    }
+    picturesItem.appendChild(fragment);
+  };
+
+  createPicturesList(window.data.pictures);
+
+  const commentsContainer = document.querySelector(`.social__comments`);
+  const commentsItem = document.querySelector(`.social__comment`);
+
+  // Создает один комментарий
+  const createCommentItem = (commentData) => {
+    const commentElement = commentsItem.cloneNode(true);
+
+    commentElement.querySelector(`.social__picture`).src = commentData.avatar;
+    commentElement.querySelector(`.social__picture`).alt = commentData.name;
+    commentElement.querySelector(`.social__text`).textContent = commentData.message;
+
+    return commentElement;
+  };
+
+  // Создает и заполняет DOM-элементы
+  const createCommentsFragment = (commentData) => {
+    commentsContainer.innerHTML = ``;
+    const fragment = document.createDocumentFragment();
+    for (let i = 0; i < commentData.length; i++) {
+      fragment.appendChild(createCommentItem(commentData[i]));
+    }
+    commentsContainer.appendChild(fragment);
+  };
+
+  window.gallery = {
+    createCommentsFragment: createCommentsFragment
+  };
+})();
---
+++ js/validation.js
@@ -0,0 +1,45 @@
+'use strict';
+
+(() => {
+  const HashtagsLength = {
+    MIN: 2,
+    MAX: 20,
+  };
+  const HASHTAG_MAX_AMOUNT = 5;
+
+  // Валидация хеш-тегов
+  const hasSymbols = (symbol) => symbol.match(/^#[a-zA-Z0-9а-яА-Я]+$/);
+
+  const checkDuplicateHashtags = (hashtags, hashtag) => {
+    const index = hashtags.indexOf(hashtag) + 1;
+    return hashtags.indexOf(hashtag, index);
+  };
+
+  const getValidityMessages = (hashtags) => {
+    if (hashtags.length > HASHTAG_MAX_AMOUNT) {
+      return `нельзя указать больше ${HASHTAG_MAX_AMOUNT} хэш-тегов`;
+    }
+    for (let hashtag of hashtags) {
+      if (hashtag[0] !== `#`) {
+        return `хэш-тег начинается с символа # (решётка)`;
+      } else if (hashtag.length < HashtagsLength.MIN) {
+        return `хеш-тег не может состоять только из одной решётки`;
+      } else if (!hasSymbols(hashtag)) {
+        return `строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.`;
+      } else if (hashtag.length > HashtagsLength.MAX) {
+        return `максимальная длина одного хэш-тега ${HashtagsLength.MAX} символов, включая решётку`;
+      } else if (checkDuplicateHashtags(hashtags, hashtag) !== -1) {
+        return `один и тот же хэш-тег не может быть использован дважды`;
+      }
+    }
+    return ``;
+  };
+
+  const hashtagInputHandler = (evt) => {
+    const hashtags = evt.target.value.toLowerCase().split(` `);
+    evt.target.setCustomValidity(getValidityMessages(hashtags));
+  };
+  window.validation = {
+    hashtagInputHandler: hashtagInputHandler
+  };
+})();
