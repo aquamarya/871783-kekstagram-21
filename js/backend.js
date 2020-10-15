a9613f58ce8c8ed1861f8ca1712a43c13ca3a10e
Maria Vlasova <vlasova_ya@mail.ru>
2020-10-16 00:38:30 +0400

    module6-task2
    adds post request

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,8 @@
 'use strict';
 
 (() => {
-  const URL = `https://21.javascript.pages.academy/kekstagram/data`;
+  const URL_GET = `https://21.javascript.pages.academy/kekstagram/data`;
+  const URL_POST = `https://21.javascript.pages.academy/kekstagram`;
   const StatusCode = {
     OK: 200,
     BAD_REQUEST: 400,
@@ -9,10 +10,9 @@
     ETERNAL_SERVER_ERROR: 500
   };
   const TIMEOUT = 10000;
+  const mainBlock = document.querySelector(`main`);
 
-  const load = (onSuccess, onError) => {
-    const xhr = new XMLHttpRequest();
-
+  const createResponse = (xhr, onSuccess, onError) => {
     xhr.responseType = `json`;
 
     xhr.addEventListener(`load`, function () {
@@ -30,7 +30,6 @@
         case StatusCode.ETERNAL_SERVER_ERROR:
           error = `Ничего не найдено`;
           break;
-
         default:
           error = `Статус ответа: ${xhr.status} ${xhr.statusText}`;
       }
@@ -50,11 +49,45 @@
 
     xhr.TIMEOUT = TIMEOUT;
 
-    xhr.open(`GET`, URL);
+    return xhr;
+  };
+
+  const load = (onSuccess, onError) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(`GET`, URL_GET);
+    createResponse(xhr, onSuccess, onError);
     xhr.send();
   };
 
-  const errorHandler = (errorMessage) => {
+  const send = function (data, onSuccess, onError) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(`POST`, URL_POST);
+    createResponse(xhr, onSuccess, onError);
+    xhr.send(data);
+  };
+
+  const renderMessage = (message, type) => {
+    mainBlock.appendChild(message);
+    const closeButton = mainBlock.querySelector(`.${type}__button`);
+    const popup = mainBlock.querySelector(`.${type}`);
+
+    closeButton.addEventListener(`click`, () => {
+      popup.remove();
+    });
+    document.addEventListener(`keydown`, (evt) => {
+      if (evt.key === window.util.ESC_KEY) {
+        popup.remove();
+      }
+    });
+    document.addEventListener(`mouseup`, (evt) => {
+      const target = evt.target.closest(`.${type}__inner`);
+      if (!target) {
+        popup.remove();
+      }
+    });
+  };
+
+  const loadErrorHandler = (errorMessage) => {
     const node = document.createElement(`div`);
     node.style = `z-index: 100; margin: 0 auto; text-align: center; background-color: #ff4e4e; border: 2px solid white;`;
     node.style.position = `absolute`;
@@ -66,8 +99,25 @@
     document.body.insertAdjacentElement(`afterbegin`, node);
   };
 
+  const errorHandler = () => {
+    const templateErrorMessage = document.querySelector(`#error`).content.querySelector(`.error`);
+    const messageElement = templateErrorMessage.cloneNode(true);
+
+    renderMessage(messageElement, `error`);
+  };
+
+  const successHandler = () => {
+    const templateSuccessMessage = document.querySelector(`#succes`).content.querySelector(`.success`);
+    const messageElement = templateSuccessMessage.cloneNode(true);
+
+    renderMessage(messageElement, `success`);
+  };
+
   window.backend = {
     load,
-    errorHandler
+    send,
+    loadErrorHandler,
+    errorHandler,
+    successHandler
   };
 })();
