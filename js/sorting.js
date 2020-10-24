fd87dbec9234e26d27e0458755730f74ab36bb46
Maria Vlasova <vlasova_ya@mail.ru>
2020-10-24 22:39:52 +0400

    module7-task1
    fixes bugs

--- js/sorting.js
+++ js/sorting.js
@@ -3,9 +3,6 @@
 (() => {
   const RANDOM_PHOTOS_AMOUNT = 10;
   const imgFiltersElement = document.querySelector(`.img-filters`);
-  const filtersDefault = imgFiltersElement.querySelector(`#filter-default`);
-  const filtersRandom = imgFiltersElement.querySelector(`#filter-random`);
-  const filtersDiscussed = imgFiltersElement.querySelector(`#filter-discussed`);
 
   imgFiltersElement.classList.remove(`img-filters--inactive`);
 
@@ -17,9 +14,7 @@
     filterButton.classList.add(`img-filters__button--active`);
   };
 
-  const getDefaultPhotos = () => {
-    return window.gallery.usersPhotos;
-  };
+  const getDefaultPhotos = () => window.gallery.usersPhotos;
 
   const getRandomPhotos = () => {
     return window.util.shufflePhotos(window.gallery.usersPhotos.slice().splice(0, RANDOM_PHOTOS_AMOUNT));
@@ -35,19 +30,23 @@
   const filterClickHandler = window.util.debounce((evt) => {
     changeActiveFilters(evt.target);
     window.gallery.removePhotos();
-    switch (evt.target) {
-      case filtersDefault :
-        window.gallery.createPicturesList(getDefaultPhotos());
+    let sortedPhotos = [];
+
+    switch (evt.target.id) {
+      case `filter-default`:
+        sortedPhotos = getDefaultPhotos();
         break;
-      case filtersRandom:
-        window.gallery.createPicturesList(getRandomPhotos());
+      case `filter-random`:
+        sortedPhotos = getRandomPhotos();
         break;
-      case filtersDiscussed:
-        window.gallery.createPicturesList(getDiscussedPhotos());
+      case `filter-discussed`:
+        sortedPhotos = getDiscussedPhotos();
         break;
       default:
-        getDefaultPhotos();
+        sortedPhotos = getDefaultPhotos();
     }
+
+    window.gallery.createPicturesList(sortedPhotos);
   });
 
   imgFiltersElement.addEventListener(`click`, filterClickHandler);
