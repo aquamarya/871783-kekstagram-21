a0de6bd343b481f793e2eeed11e3f96adfc02d78
Maria Vlasova <vlasova_ya@mail.ru>
2020-11-02 18:26:10 +0400

    module9-task1
    adds photo`s upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require(`path`);
-
-module.exports = {
-  entry: [
-    `./js/util.js`,
-    `./js/sorting.js`,
-    `./js/backend.js`,
-    `./js/gallery.js`,
-    `./js/preview.js`,
-    `./js/editor.js`,
-    `./js/validation.js`,
-    `./js/upload-photo.js`,
-    `./js/form.js`,
-    `./js/slider.js`,
-  ],
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname),
-    iife: true
-  },
-  devtool: false
-};
+// const path = require(`path`);
+//
+// module.exports = {
+//   entry: [
+//     `./js/util.js`,
+//     `./js/sorting.js`,
+//     `./js/backend.js`,
+//     `./js/gallery.js`,
+//     `./js/preview.js`,
+//     `./js/editor.js`,
+//     `./js/validation.js`,
+//     `./js/form.js`,
+//     `./js/upload-photo.js.js`,
+//     `./js/slider.js`,
+//   ],
+//   output: {
+//     filename: "bundle.js",
+//     path: path.resolve(__dirname),
+//     iife: true
+//   },
+//   devtool: false
+// };
